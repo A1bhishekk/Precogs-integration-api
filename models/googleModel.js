9030b03709734fc8940f9af331392754b0ef7d8c
Abhishek Kumar <sahniak56@gmail.com>
2024-01-10 15:10:23 +0530

    g

--- models/googleModel.js
+++ models/googleModel.js
@@ -38,59 +38,58 @@ module.exports = GoogleUser;
 
 
 
-const mongoose = require("mongoose");
-const projectSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        full_name: {
-            type: String,
-        },
-        githubLink: {
-            type: String,
-        },
-        html_url: {
-            type: String,
-        },
-        id: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        language: {
-            type: String,
-        },
-        businessPriority: {
-            type: Map,
-            of: String,
-        },
+// const projectSchema = new mongoose.Schema(
+//     {
+//         name: {
+//             type: String,
+//             required: true,
+//         },
+//         full_name: {
+//             type: String,
+//         },
+//         githubLink: {
+//             type: String,
+//         },
+//         html_url: {
+//             type: String,
+//         },
+//         id: {
+//             type: String,
+//             required: true,
+//             unique: true,
+//         },
+//         language: {
+//             type: String,
+//         },
+//         businessPriority: {
+//             type: Map,
+//             of: String,
+//         },
 
-        projectScope: {
-            type: Map,
-            of: String,
-        },
-        environment: {
-            type: Map,
-            of: String,
-        },
-        projectType: {
-            type: Map,
-            of: String,
-        },
+//         projectScope: {
+//             type: Map,
+//             of: String,
+//         },
+//         environment: {
+//             type: Map,
+//             of: String,
+//         },
+//         projectType: {
+//             type: Map,
+//             of: String,
+//         },
 
-        owner: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-    },
-    {
-        timestamps: true,
-    }
-);
+//         owner: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User",
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
 
-const Project = mongoose.model("Project", projectSchema);
+// const Project = mongoose.model("Project", projectSchema);
 
-module.exports = Project;
+// module.exports = Project;
 
